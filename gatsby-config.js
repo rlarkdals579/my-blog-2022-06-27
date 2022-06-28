4d3f607cbc289a54e1ab472bd549bff86a610b07
rlarkdals579 <rlarkdals579@naver.com>
2022-06-28 16:04:32 +0900

    blog mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
       resolve: "gatsby-source-filesystem",
       options: {
         name: `blog`,
-        path: `${__dirname}/blog`,
+        path: `${__dirname}/pages`,
       },
     },
   ],
